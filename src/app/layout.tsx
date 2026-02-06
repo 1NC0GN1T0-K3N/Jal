b1c410a5af4e173b459f042093bd1cf3995eb924
1NC0GN1T0-K3N <lionthiha10@gmail.com>
2026-02-06 15:55:52 +0700

    Final Update for Jal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Valentines: A Romantic Card Game with a Surprise Proposal",
+  title: "For my Pookie Jal",
   description:
-    "Play a unique Valentine's card game. Complete the collection to reveal a romantic proposal!",
+    "A lil game for a special someone <3",
   keywords: [
     "Valentine's card game",
     "romantic proposal game",
